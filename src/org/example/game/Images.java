b98decad6673903174ee346ad44caeed0057f4b0
guo543 <guo543@purdue.edu>
2021-06-03 13:20:10 +0800

    edited Images class

--- src/org/example/game/Images.java
+++ src/org/example/game/Images.java
@@ -1,19 +1,27 @@
 package org.example.game;
 
 import javax.swing.*;
+import java.io.File;
 import java.net.URL;
 
 public class Images {
+    public static ImageIcon bodyImg;
 
-    public static URL bodyUrl = Images.class.getResource("/images/body.jpg");
+    public static ImageIcon headImg;
 
-    public static ImageIcon bodyImg = new ImageIcon(bodyUrl);
+    public static ImageIcon foodImg;
 
-    public static URL headUrl = Images.class.getResource("/images/head.jpg");
+    static {
+        File head = new File("images/head.jpg");
+        String headPath = head.getAbsolutePath();
+        headImg = new ImageIcon(headPath);
 
-    public static ImageIcon headImg = new ImageIcon(headUrl);
+        File body = new File("images/body.jpg");
+        String bodyPath = body.getAbsolutePath();
+        bodyImg = new ImageIcon(bodyPath);
 
-    public static URL foodUrl = Images.class.getResource("/images/food.jpg");
-
-    public static ImageIcon foodImg = new ImageIcon(foodUrl);
+        File food = new File("images/food.jpg");
+        String foodPath = food.getAbsolutePath();
+        foodImg = new ImageIcon(foodPath);
+    }
 }
